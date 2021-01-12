22c5177d65d8f43ff27242ba8eb243c105e24e9e
Mikhailov Alexey <55998890+Saviorich@users.noreply.github.com>
2021-01-12 20:48:02 +0300

    Add files via upload

--- src/main/java/by/epam/tc/reader/Reader.java
+++ src/main/java/by/epam/tc/reader/Reader.java
@@ -5,25 +5,3 @@ import java.io.IOException;
 public interface Reader {
     Object readFile(String path) throws IOException;
 }
-
-class A {
-
-    {
-        System.out.println("First");
-    }
-
-    {
-        System.out.println("Second");
-    }
-
-    void m() {
-    }
-
-    {
-        System.out.println("Third");
-    }
-
-    public static void main(String[] args) {
-        new A();
-    }
-}
